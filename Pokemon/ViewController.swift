585b288792627ceb6a0627273e0abef7997b0c19
Peyman Attarzadeh <peymanium@Peymans-Macbook.local>
2016-05-25 01:30:55 +0400

    Add search feature

--- Pokemon/ViewController.swift
+++ Pokemon/ViewController.swift
@@ -8,19 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate
 {
 
     @IBOutlet weak var collectionView : UICollectionView!
-    
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var BTN_Music: UIButton!
+    
     var pokemonArray = [Pokemon]()
+    var pokemonSearch = [Pokemon]()
+    var isSearchMode = false
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
+        self.searchBar.delegate = self
+        
+        self.searchBar.returnKeyType = .Done
         
         Functions.instance.InitMusic()
         pokemonArray = Functions.instance.ParseCSV()
@@ -28,17 +35,34 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     
     //CollectionView delegates
-    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int
+    {
         return 1
     }
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        if isSearchMode
+        {
+            return pokemonSearch.count
+        }
+        
         return pokemonArray.count
     }
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell
+    {
         
         if let cell = self.collectionView.dequeueReusableCellWithReuseIdentifier("CellID", forIndexPath: indexPath) as? PokemonCell
         {
-            let pokemon = pokemonArray[indexPath.row]
+            let pokemon : Pokemon!
+            if isSearchMode
+            {
+                pokemon = pokemonSearch[indexPath.row]
+            }
+            else
+            {
+                pokemon = pokemonArray[indexPath.row]
+            }
+           
             cell.ConfigureCell(pokemon)
             
             return cell
@@ -49,14 +73,39 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         }
         
     }
-    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath)
+    {
         
     }
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize
+    {
         return CGSizeMake(100, 100)
     }
     
     
+    //Search bar delegate
+    func searchBarSearchButtonClicked(searchBar: UISearchBar)
+    {
+        view.endEditing(true)
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String)
+    {
+        if self.searchBar.text == nil || self.searchBar.text == ""
+        {
+            self.isSearchMode = false
+            view.endEditing(true)
+        }
+        else
+        {
+            self.isSearchMode = true
+            let lowerSearchString = self.searchBar.text!.lowercaseString
+            pokemonSearch = pokemonArray.filter({$0.pokemonName.rangeOfString(lowerSearchString) != nil})
+        }
+        self.collectionView.reloadData()
+    }
+    
+    //Music button
     @IBAction func BTN_Music_Tapped(sender: AnyObject)
     {
         if Functions.instance.musicPlayer.playing
