d8ee64620fadb7ab2d671f45c0a73e8c0390aed9
Peyman Attarzadeh <peymanium@Peymans-Macbook.local>
2016-05-24 19:31:15 +0400

    Created the storyboard

--- Pokemon/ViewController.swift
+++ Pokemon/ViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
+{
 
+    @IBOutlet weak var collectionView : UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
     }
-
+    
+    
+    //CollectionView delegates
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        if let cell = self.collectionView.dequeueReusableCellWithReuseIdentifier("CellID", forIndexPath: indexPath) as? PokemonCell
+        {
+            let pokemon = Pokemon(name: "Charzaar", id: indexPath.row)
+            cell.ConfigureCell(pokemon)
+            
+            return cell
+        }
+        else
+        {
+            return PokemonCell()
+        }
+        
+    }
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake(100, 100)
+    }
+    
+    
 
 }
 
